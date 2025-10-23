cfbbc878e3e195443b94e24025520fa70afe151d
estno <estno@ANGELHERO>
2025-10-23 16:29:06 -0400

    Add project files.

---
+++ source.cpp
@@ -0,0 +1,17 @@
+//Espoir Ndayisabye
+//CIS 1202
+//source code assignements
+
+
+#include<iostream>
+
+
+using namespace std;
+
+int main() {
+	string name = "Hello World";
+
+	cout << name << endl;
+
+	cout << endl << endl;
+}
